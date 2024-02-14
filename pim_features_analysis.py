ca7c03228ca36dd18344ac399c8d124b7a72cef0
George Murgă <george.murga+github@gmail.com>
2024-02-14 13:08:22 +0200

    Cleanup some detasils.

--- pim_features_analysis.py
+++ pim_features_analysis.py
@@ -11,6 +11,12 @@ from typing import Union
 __version__ = "0.1.0"
 
 
+def main() -> None:
+    """Main function."""
+    args: argparse.Namespace = parse_args()
+    analyze_features(args.input_csv)
+
+
 def parse_args() -> argparse.Namespace:
     """Parse command line arguments."""
     parser = argparse.ArgumentParser(prog="pim_feature_analyis", description=__doc__)
@@ -23,13 +29,6 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def main() -> None:
-    """Main function."""
-    args: argparse.Namespace = parse_args()
-    print(args)
-    analyze_features(args.input_csv)
-
-
 def analyze_features(input_csv: Union[str, Path]) -> None:
     """Analyze the features."""
     if isinstance(input_csv, str):
@@ -38,18 +37,20 @@ def analyze_features(input_csv: Union[str, Path]) -> None:
         raise FileNotFoundError(f"File {input_csv} not found.")
     output_csv: Path = input_csv.parent / f"{input_csv.stem}_analyzed.csv"
 
-    forbidden_chars: Pattern[str] = re.compile(r"[^a-zA-Z0-9_\-]")
+    forbidden_chars: Pattern[str] = re.compile(r"[^a-zA-Z0-9_-]")
 
     with input_csv.open("r", encoding="utf-8") as csv_file, output_csv.open(
         "w", encoding="utf-8", newline=""
     ) as out_file:
         reader = csv.DictReader(csv_file)
         header: Sequence[str] | None = reader.fieldnames
+
         if header is None:
             raise ValueError(f"No header found in input file: {input_csv.as_posix()}")
+
         writer = csv.writer(out_file)
         writer.writerow(header)
-        count_of_invalid_features: int = 0
+
         for row in reader:
             found_forbidden_chars: List[str] | None = forbidden_chars.findall(
                 row["Identifier"]
@@ -58,7 +59,6 @@ def analyze_features(input_csv: Union[str, Path]) -> None:
                 print(
                     f"Found forbidden characters in {row['Identifier']}: {found_forbidden_chars}"
                 )
-                count_of_invalid_features += 1
                 writer.writerow(row.values())
 
 
